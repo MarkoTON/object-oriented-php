ea8847f734c456aff7d6a5d26a7801daa07c67ac
Marko <29685366+MarkoTON@users.noreply.github.com>
2021-11-29 12:37:35 +0100

    adding comments

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php 
 
   class User {
-
+    // properties methods
   }
 
   $userOne = new User();
   $userTwo = new User();
 
-  echo 'the class is: ' . get_class($userOne);
+  echo 'the class is: ' . get_class($userOne); // User - vraca naziv classe
+  echo "<br>";
+  echo 'the class is: ' . get_class($userTwo); // User - vraca naziv classe
 
 ?>
 
